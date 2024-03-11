512dec1b10e4fc7b3c48419b51ffe9e4d8a51dfe
BearNUNU <kkw31025@gmail.com>
2024-03-12 00:12:28 +0900

    CH.5 : vueData1
    v-on, handler

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@
     <!-- <lifeCycle /> -->
     <!-- <userInterface1/> -->
     <!-- <userInterface2/> -->
-    <userInterface3/>
+    <!-- <userInterface3/> -->
+    <vueData1 />
   </div>
 </template>
 
 <script setup>
-  import lifeCycle from "./components/lifeCycle.vue";
-  import userInterface1 from "./components/userInterface1.vue";
-  import userInterface2 from "./components/userInterface2.vue";
-  import userInterface3 from "./components/userInterface3.vue"
+import lifeCycle from "./components/lifeCycle.vue"
+import userInterface1 from "./components/userInterface1.vue"
+import userInterface2 from "./components/userInterface2.vue"
+import userInterface3 from "./components/userInterface3.vue"
+import vueData1 from "./components/vueData1.vue"
 </script>
 
-
-<style>
-</style>
+<style></style>
--- src/components/userInterface3.vue
+++ src/components/userInterface3.vue
@@ -1,16 +1,19 @@
 <template>
-    <div>
-        <li>{{sampleArr[0]}}</li>
-      <li v-for="item in sampleArr" :key="item">{{ item }}</li>
-      <li v-for="user in userArr" :key="user.id">{{user.id + user.name}}</li>
-    </div>
+  <div>
+    <li>{{ sampleArr[0] }}</li>
+    <li v-for="item in sampleArr" :key="item">{{ item }}</li>
+    <li v-for="user in userArr" :key="user.id">{{ user.id + user.name }}</li>
+  </div>
 </template>
 
 <script setup>
-const sampleArr = ['a', 'b','c','d']
-const userArr = [{id:1, name:'a'},{id:2, name:'b'},{id:3, name:'c'},{id:4, name:'d'}]
+const sampleArr = ["a", "b", "c", "d"]
+const userArr = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+  { id: 3, name: "c" },
+  { id: 4, name: "d" },
+]
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
