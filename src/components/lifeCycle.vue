736307517ac94572181c21e2046248d957890dab
DESKTOP-F3JGV25\BLACKBERRY <kkw31025@gmail.com>
2024-03-11 14:44:47 +0900

    CH.1: LifeCycle hooks
    lifeCycle.vue

---
+++ src/components/lifeCycle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+{{count}}
+  </div>
+  <h1>텍스트</h1>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      count: 0,
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate', this.count);
+    //beforeCreate undefined
+  },
+  created(){
+    console.log('create', this.count);
+// create 0
+  },
+  beforeMount(){
+  console.log('beforeMount', document.querySelector('h1'));
+  },  
+  mounted(){
+  console.log('mounted', document.querySelector('h1'));
+  },
+  methods(){
+   test(
+    console.log('test')
+   )
+   },
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
